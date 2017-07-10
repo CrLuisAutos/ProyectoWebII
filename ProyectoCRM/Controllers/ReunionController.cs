ae65f463976d1f18d9fd456a55879c1648544654
Luis Espinoza <espinozaluis3680@gmail.com>
2017-07-10 17:03:30 -0600

    Reunion

---
+++ ProyectoCRM/Controllers/ReunionController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoCRM.Models;
+
+namespace ProyectoCRM.Controllers
+{
+    public class ReunionController : Controller
+    {
+        private CRMDB db = new CRMDB();
+
+        // GET: Reunion
+        public ActionResult Index()
+        {
+            return View(db.Reunion.ToList());
+        }
+
+        // GET: Reunion/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Reunion reunion = db.Reunion.Find(id);
+            if (reunion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(reunion);
+        }
+
+        // GET: Reunion/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Reunion/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,titulo,fecha,_virtual")] Reunion reunion)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Reunion.Add(reunion);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(reunion);
+        }
+
+        // GET: Reunion/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Reunion reunion = db.Reunion.Find(id);
+            if (reunion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(reunion);
+        }
+
+        // POST: Reunion/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,titulo,fecha,_virtual")] Reunion reunion)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(reunion).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(reunion);
+        }
+
+        // GET: Reunion/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Reunion reunion = db.Reunion.Find(id);
+            if (reunion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(reunion);
+        }
+
+        // POST: Reunion/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Reunion reunion = db.Reunion.Find(id);
+            db.Reunion.Remove(reunion);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
